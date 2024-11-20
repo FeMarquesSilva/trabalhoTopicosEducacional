fdd6bde2d53b6d86f928d38e68752deeda3812b0
FeMarquesSilva <felipemarques330@gmail.com>
2024-11-20 13:22:40 -0300

    Criando a modal de matriculas

---
+++ src/main/java/br/grupointegrado/projetoTDE/model/Matricula.java
@@ -0,0 +1,42 @@
+package br.grupointegrado.projetoTDE.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "matriculas")
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "aluno_id")
+    private Integer alunoId;
+
+    @Column(name = "turma_id")
+    private Integer turmaId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(Integer alunoId) {
+        this.alunoId = alunoId;
+    }
+
+    public Integer getTurmaId() {
+        return turmaId;
+    }
+
+    public void setTurmaId(Integer turmaId) {
+        this.turmaId = turmaId;
+    }
+}
