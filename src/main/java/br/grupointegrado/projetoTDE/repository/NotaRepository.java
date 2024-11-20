c8fc72a1c610805a7fe0476924b57ecd30fd0186
FeMarquesSilva <felipemarques330@gmail.com>
2024-11-20 14:19:25 -0300

    Adicionando a modal e repositry de nota

---
+++ src/main/java/br/grupointegrado/projetoTDE/repository/NotaRepository.java
@@ -0,0 +1,7 @@
+package br.grupointegrado.projetoTDE.repository;
+
+import br.grupointegrado.projetoTDE.model.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotaRepository extends JpaRepository<Nota, Integer> {
+}
