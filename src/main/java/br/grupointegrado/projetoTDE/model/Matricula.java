016648e1ab9ac963687d06f8b1d0c370a3f9271e
FeMarquesSilva <felipemarques330@gmail.com>
2024-11-22 21:01:40 -0300

    Removendo o relatorio para refazer, pois estava dando bug em outros componentes

--- src/main/java/br/grupointegrado/projetoTDE/model/Matricula.java
+++ src/main/java/br/grupointegrado/projetoTDE/model/Matricula.java
@@ -9,12 +9,12 @@ public class Matricula {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    
     @Column(name = "aluno_id")
-    private Aluno alunoId;
+    private Integer alunoId;
 
     @Column(name = "turma_id")
-    private Turma turmaId;
+    private Integer turmaId;
 
     public Integer getId() {
         return id;
@@ -24,19 +24,19 @@ public class Matricula {
         this.id = id;
     }
 
-    public Aluno getAlunoId() {
+    public Integer getAlunoId() {
         return alunoId;
     }
 
-    public void setAlunoId(Aluno alunoId) {
+    public void setAlunoId(Integer alunoId) {
         this.alunoId = alunoId;
     }
 
-    public Turma getTurmaId() {
+    public Integer getTurmaId() {
         return turmaId;
     }
 
-    public void setTurmaId(Turma turmaId) {
+    public void setTurmaId(Integer turmaId) {
         this.turmaId = turmaId;
     }
 }
